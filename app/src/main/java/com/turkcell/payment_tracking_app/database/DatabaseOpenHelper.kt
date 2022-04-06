87eca5cd0ca21da00113e78f436ffad64cb5ace5
ibrahimertanylmz <ibrahimertanylmz@gmail.com>
2022-04-06 16:40:51 +0300

    db onDeleteCascade operation added

--- app/src/main/java/com/turkcell/payment_tracking_app/database/DatabaseOpenHelper.kt
+++ app/src/main/java/com/turkcell/payment_tracking_app/database/DatabaseOpenHelper.kt
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper
 class DatabaseOpenHelper(context: Context, name: String, factory: SQLiteDatabase.CursorFactory?, version: Int) : SQLiteOpenHelper(context, name, factory, version){
     override fun onCreate(db: SQLiteDatabase) {
         val sorguForeingKey = "PRAGMA foreign_keys = ON"
-        db!!.execSQL(sorguForeingKey)
+        db.execSQL(sorguForeingKey)
         val sorguPaymentType = "CREATE TABLE PaymentType(Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Title TEXT, Period TEXT, PeriodDay INTEGER)"
         db.execSQL(sorguPaymentType)
         val sorguPayment = "CREATE TABLE Payment(Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Date TEXT, Price INTEGER, PaymentTypeId INTEGER, FOREIGN KEY(PaymentTypeId) REFERENCES PaymentType(Id) ON DELETE CASCADE)"
